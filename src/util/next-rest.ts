d6c0ededa7f1249d698ecef40301add0ba15204a
Theta <github@partheybhatt.com>
2023-04-01 18:15:18 -0700

    Initial commit

---
+++ src/util/next-rest.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const is =
+  <T extends z.ZodTypeAny>(schema: T) =>
+  (value: unknown): value is z.infer<T> => {
+    const result = schema.safeParse(value);
+
+    return result.success;
+  };
