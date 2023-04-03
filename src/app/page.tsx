a1ecc7f013ead81ef9966c12020ef27295372828
Parthey Bhatt <PBhatt@raa.com.au>
2023-04-03 13:26:29 +0930

    Removed .env, added metadata-scraper for getting link metadata, added db migration file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function RootPage() {
-  return <h1 className="text-4xl font-bold">Hello World!</h1>;
+import api from "@theta-cubed/next-rest/client"
+
+const getLinks = async () => {
+  const response = await api.get("/api/links", { headers: {}, params: {}, body: undefined });
+
+  return response.body.links;
+}
+
+export default async function RootPage() {
+  const links = await getLinks();
+
+  return (
+    <div className="flex flex-col gap-y-2">{
+      links.map((link, index) => (
+        <span className="text-sm">{link.title}</span>
+      ))
+    }</div>
+  );
 }
---
+++ src/db/migrations/2023-04-03T01.58.47-adding-more-columns-to-link.mts
@@ -0,0 +1,17 @@
+import { Kysely } from 'kysely';
+
+export const up = async (db: Kysely<any>) => {
+	await db.schema
+		.alterTable('link')
+		.addColumn('description', 'varchar', (col) => col.notNull())
+		.addColumn('image', 'varchar', (col) => col.notNull())
+		.addColumn('icon', 'varchar', (col) => col.notNull());
+};
+
+export const down = async (db: Kysely<any>) => {
+	await db.schema
+		.alterTable('link')
+		.dropColumn('description')
+		.dropColumn('image')
+		.dropColumn('icon');
+};
